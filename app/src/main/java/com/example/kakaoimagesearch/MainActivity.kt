6ec7221ad1effe822bd0d271ac727a228980e321
rihan530 <renovatio530@gmail.com>
2024-01-29 21:18:23 +0900

    이미지 검색 레이아웃 생성

--- app/src/main/java/com/example/kakaoimagesearch/MainActivity.kt
+++ app/src/main/java/com/example/kakaoimagesearch/MainActivity.kt
@@ -1,11 +1,84 @@
 package com.example.kakaoimagesearch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.kakaoimagesearch.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentDataListener {
+
+    // 하단의 뒤로가기 버튼을 눌렀을 때 종료 확인 다이얼로그가 뜨는 콜백 함수
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+
+        }
+    }
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var searchListFragment: SearchListFragment
+
+    val adapter = ViewPagerAdapter(this)
+
+    override fun onStart() {
+        super.onStart()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions(arrayOf(
+                Manifest.permission.INTERNET
+            ), 0)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        this.onBackPressedDispatcher.addCallback(this, callback)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions(arrayOf(
+                Manifest.permission.POST_NOTIFICATIONS,
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.READ_MEDIA_IMAGES
+            ), 0)
+        }
+
+        window.apply {
+            // 상태바의 아이콘과 배경색 변경
+            statusBarColor = Color.WHITE
+            WindowInsetsControllerCompat(this, this.decorView).isAppearanceLightStatusBars = true
+        }
+
+        with(binding) {
+            viewPagerMain.adapter = adapter
+
+            TabLayoutMediator(tabLayoutMainBottom, viewPagerMain) { tab, position ->
+                when (position) {
+                    0 -> tab.text = "이미지 검색"/*getString(R.string.contact_list_tab)*/
+                    1 -> tab.text = "내 보관함"/*getString(R.string.my_page_tab)*/
+                }
+            }.attach()
+        }
     }
+
+//    override fun update(position: Int) {
+//        adapter.updateLike(position)
+//    }
+
+//    override fun onDataReceived(data: Contact.Person) {
+//        supportFragmentManager.commit {
+//            detailFragment = ContactDetailFragment.newInstance(data)
+//            replace(R.id.frame_layout_main, detailFragment)
+//            setReorderingAllowed(true)
+//            addToBackStack("")
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/kakaoimagesearch/SearchListFragment.kt
@@ -0,0 +1,64 @@
+package com.example.kakaoimagesearch
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kakaoimagesearch.placeholder.PlaceholderContent
+
+interface FragmentDataListener {
+//    fun onDataReceived(data: Contact.Person)
+}
+interface UpdateLike {
+    fun update(position: Int)
+}
+class SearchListFragment : Fragment() {
+
+    private var columnCount = 2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_search_list_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = SearchRVAdapter(PlaceholderContent.ITEMS)
+            }
+        }
+        return view
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            SearchListFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
+    }
+}
